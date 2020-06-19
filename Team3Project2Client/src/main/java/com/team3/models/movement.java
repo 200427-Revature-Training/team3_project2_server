de1bb61c93ec535b67f8ece3e7df384ff5751fc5
Anti Evo <jpujals@bellsouth.net>
2020-06-19 18:01:13 -0400

    skeleton server

---
+++ Team3Project2Client/src/main/java/com/team3/models/movement.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team3.models;
+
+import java.time.LocalDate;
+import java.util.List;
+import com.team3.models.user;
+/**
+ *
+ * @author JJ
+ */
+
+
+
+public class movement {
+    public movement(int id, String goal, LocalDate start, LocalDate expire, String desc, user author, user approver, int status, int type) {
+		super();
+		
+	}
+
+	public movement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
